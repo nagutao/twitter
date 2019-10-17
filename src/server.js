782ddf307b84651edf97e3eaefec64c7c30a8dbd
nagutao <guilherme_tonello@hotmail.com>
2019-10-17 11:56:00 -0300

    ultimos ajustes

--- src/server.js
+++ src/server.js
@@ -1,18 +1,28 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 mongoose.connect('mongodb+srv://nagutao:nagutao@cluster0-ebldr.gcp.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+app.use((req, res, next)=>{
+    req.io = io;
+    next();
+})
+
 app.use(cors());
 
+app.use('/tweet', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
+
 app.use(express.json());
-// app.use(express.urlencoded());
 
 app.use(require('./routes'));
 
-app.listen(4000);
+server.listen(4000);
